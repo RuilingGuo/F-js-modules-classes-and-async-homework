9ae5109ff9ee352644f65a23796872c98f8dd527
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-14 23:00:21 +0800

    [ruiling]feat: finish practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,12 @@
-export const getPoetry
+async function getPoetry() {
+  let result = [];
+  await fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then((data) => {
+      result = [data.origin, data.author, data.content];
+    })
+    .catch((err) => console.log(err));
+  return result;
+}
+
+export { getPoetry };
